330ba312897eaa07de8ade654e966a1fbfcc0d9e
AnuragTiwari1 <anuragkumartiwari79@gmail.com>
2018-12-28 00:57:51 +0530

    added support to read open port on machine

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -1,5 +1,32 @@
+import com.fazecast.jSerialComm.SerialPort;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class MainApplication {
     public static void main(String[] args){
-        System.out.println("Hello WOrld");
+
+        SerialPort[] openPorts = SerialPort.getCommPorts();
+        for (SerialPort s:
+             openPorts) {
+            System.out.println("open port>>>>"+s.getSystemPortName()+" serving>>>"+s.getDescriptivePortName());
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter serial port to open (e.g., /dev/ttyS0 or COM3)::");
+        String port = scanner.nextLine();
+        SerialPort comPort = SerialPort.getCommPort(port);
+        comPort.openPort();
+        try {
+            while (true)
+            {
+                while (comPort.bytesAvailable() == 0)
+                    Thread.sleep(20);
+
+                byte[] readBuffer = new byte[comPort.bytesAvailable()];
+                System.out.println("arduino says>>>>>"+new String(readBuffer));
+            }
+        } catch (Exception e) { e.printStackTrace(); }
+        comPort.closePort();
     }
 }
